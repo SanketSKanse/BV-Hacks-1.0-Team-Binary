1c3d8ecde30280fc99f6a1463b98dbb80bdbd373
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-03-28 13:30:35 +0530

    solved too many

---
+++ user/scripts/settings.js
@@ -0,0 +1,10 @@
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+// Add this to your settings page HTML:
+// <button onclick="toggleTheme()">Toggle Theme</button>
